b124a18df37ea92c88a403dc2592314f1c96cd90
NISHANTHKELOTH <kelothnishanth7@gmail.com>
2026-08-28 22:45:19 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5002/api"
+    baseURL: "https://mini-auction-l8xd.onrender.com/api"
 });
 
 export default api;
